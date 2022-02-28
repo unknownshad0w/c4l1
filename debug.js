b6af7f75f8a73a6848585b7ed00930b03408dd98
Dionel Panuncia <unknownshadow634@gmail.com>
2022-02-28 17:55:08 -0500

    finished todays assignment

--- debug.js
+++ debug.js
@@ -2,5 +2,30 @@
 let cars = ["Saab", "Volvo", "BMW"]; //types of cars in array
 let a = "words"//variable made that is words
 let age = 15
-const car {type:"Fiat", model:"500", color:"white"}; //array with features of cars
+var car = {type:"Fiat", model:"500", color:"white"}; //array with features of cars
 (10 > 9) // 10 is greater than 9
+
+const person = {
+    firstName: "John",
+    lastName: "Doe",
+    fullName: function() {
+      return this.firstName + " " + this.lastName;
+    }
+  };  //explain this object function as best as you can
+  // code an example of a method using a string or number data type
+  // code an example of a method with an object or array data type
+  // write an example of a constructor
+  // write practical code that can have visible changes on an html document(example setIntrevals and .replace)
+  // lastly update your github repo by using the Git add . , git commit -m"finshed todays assignment" and lastly git push.
+  
+  //this code is about someones name it gives the first and last name and the full name is just the 1st and last name combined.
+
+var music = {mood:"nostalgic" time:"short" new:"no"}
+music.push(relaxing)
+
+
+var MyConstructor = me(home, age, likes)
+    this.home = bronx;
+    this.age = p13;
+    this.likes = videogames;
+  ;

387e73eda0a71b4ec93e5a4ec17aff73f847cee9
Dionel Panuncia <unknownshadow634@gmail.com>
2022-02-28 17:32:20 -0500

    updated code

---
+++ debug.js
@@ -0,0 +1,6 @@
+// check for bugs and tell me what kind of data type it is
+let cars = ["Saab", "Volvo", "BMW"]; //types of cars in array
+let a = "words"//variable made that is words
+let age = 15
+const car {type:"Fiat", model:"500", color:"white"}; //array with features of cars
+(10 > 9) // 10 is greater than 9
